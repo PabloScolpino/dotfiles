7c4e75bfe3164c14b7bfe2f4df3f85b6884bca58
Pablo Scolpino <pabloscolpino@gmail.com>
2018-10-04 15:45:07 -0300

    Updated git prompt theme

--- .config/git.sh
+++ .config/git.sh
@@ -2,3 +2,6 @@ git config --global core.excludesfile $HOME/.gitignore_global
 git config --global core.editor "vim"
 
 git config --global alias.up 'pull --rebase --autostash'
+
+GIT_PROMPT_THEME=Solarized_UserHost
+#GIT_PROMPT_THEME=Minimal_UserHost
