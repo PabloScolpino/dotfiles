b71b3c27a7fc145c0782ed1c631f0797dafd7f95
Pablo Scolpino <pabloscolpino@gmail.com>
2019-09-17 21:35:29 -0300

    Resolve conflict

--- .config/alias.sh
+++ .config/alias.sh
@@ -13,3 +13,7 @@ alias trw='tmate -S /tmp/tmate.sock display -p "#{tmate_ssh}"'
 alias tro='tmate -S /tmp/tmate.sock display -p "#{tmate_ssh_ro}"'
 
 alias rerun='rerun --no-notify'
+alias dc='docker-compose'
+alias dcb='docker-compose build'
+alias dce='docker-compose exec'
+alias dcu='docker-compose up --build'
