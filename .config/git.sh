99d8e32467d0fadbbde8f818b17820e37a963f0f
Pablo Scolpino <quantum@hephaestus.local>
2020-12-04 16:48:35 +0100

    Initial configuration for zsh

--- .config/git.sh
+++ .config/git.sh
@@ -2,5 +2,3 @@ git config --global core.excludesfile $HOME/.gitignore_global
 git config --global core.editor "vim"
 
 git config --global alias.up 'pull --rebase --autostash'
-
-GIT_PROMPT_THEME=Solarized_UserHost
