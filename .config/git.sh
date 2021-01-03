970242e8ba15b5040bd92a599c0170156769e7a8
Pablo Scolpino <pabloscolpino@gmail.com>
2021-01-03 16:38:33 +0100

    Restoring bash config files & some renaming

--- .config/git.sh
+++ .config/git.sh
@@ -1,4 +1,10 @@
+git config --global alias.up 'pull --rebase --autostash'
+
 git config --global core.excludesfile $HOME/.gitignore_global
 git config --global core.editor "vim"
 
-git config --global alias.up 'pull --rebase --autostash'
+git config --global user.email 'pabloscolpino@gmail.com'
+git config --global user.name 'Pablo Scolpino'
+
+# Used in bash prompt
+GIT_PROMPT_THEME=Solarized_UserHost
--- bin/bash_init_script.sh
+++ bin/bash_init_script.sh
@@ -1,5 +1,5 @@
-echo ". \$HOME/.config/source_this.sh" >> $HOME/.bashrc
-
+echo ". \$HOME/.config/source_this.bash" >> $HOME/.bashrc
+cd
 git submodule init && git submodule update
 
-. ./.bashrc
+. $HOME/.bashrc
