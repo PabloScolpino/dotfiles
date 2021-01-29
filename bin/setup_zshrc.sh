ba0670dc392c82b3ba096bd0f21d2f8e70d75618
Pablo Scolpino <pabloscolpino@gmail.com>
2021-01-29 16:12:06 +0100

    Better zsh setup

--- bin/setup_zshrc.sh
+++ bin/setup_zshrc.sh
@@ -1,6 +1,14 @@
-# TODO: I need to flesh this out, only did it once
-#       * Presto init goes here
-echo "you need to do the zsh presto init manually!"
+# https://github.com/sorin-ionescu/prezto
+
+# TODO: check if running zsh
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+chsh -s /bin/zsh
 
 echo ". \$HOME/.dotfiles/.config/source_this.zshenv" >> $HOME/.zshenv
 echo ". \$HOME/.dotfiles/.config/source_this.zshrc" >> $HOME/.zshrc
