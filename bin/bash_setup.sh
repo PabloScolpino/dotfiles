a3a51550350b2214979a4364ce81a25936b5d1d2
Pablo Scolpino <pabloscolpino@gmail.com>
2021-05-22 19:26:15 +0200

    Fix bash setup dotfile path

--- bin/bash_setup.sh
+++ bin/bash_setup.sh
@@ -1,7 +1,6 @@
-echo ". \$HOME/.dotfiles/config/source_this.bash" >> $HOME/.bashrc
-
-cd
+cd $HOME/.dotfiles
 
 git submodule init && git submodule update
 
+echo ". \$HOME/.dotfiles/config/source_this.bash" >> $HOME/.bashrc
 . $HOME/.bashrc
