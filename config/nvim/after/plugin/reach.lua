a80aeda49f8e7cb8555c888658df63d174fe98f3
Pablo Scolpino <pabloscolpino@gmail.com>
2023-06-04 15:33:25 +0000

    Configure hotkeys to switch between open buffers
    this can be used to switch between buffers in different projects, which
    is something harpoon could not do

--- config/nvim/after/plugin/reach.lua
+++ config/nvim/after/plugin/reach.lua
@@ -1,3 +1,11 @@
-require('reach').setup({
-  notifications = true
+local reach = require('reach')
+reach.setup({
+  handle = 'bufnr',              -- 'bufnr' or 'dynamic' or 'auto'
+  notifications = true,
 })
+
+vim.keymap.set('n', '<C-e>', function() reach.buffers(buffer_options) end, {})
+vim.keymap.set('n', '<C-j>', function() reach.switch_to_buffer(1, options) end, {})
+vim.keymap.set('n', '<C-k>', function() reach.switch_to_buffer(2, options) end, {})
+vim.keymap.set('n', '<C-l>', function() reach.switch_to_buffer(3, options) end, {})
+vim.keymap.set('n', '<C-;>', function() reach.switch_to_buffer(4, options) end, {})
