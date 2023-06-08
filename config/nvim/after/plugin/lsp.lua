0f101c83d099c09ae58d035e693e7cec3e05a03a
Pablo Scolpino <pabloscolpino@gmail.com>
2023-06-08 20:38:09 -0300

    Configure snippets on nvim

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,25 +1,25 @@
 local lsp = require('lsp-zero')
-lsp.preset("recommended")
+lsp.preset('recommended')
 
 ------------------------------------------------------------------
 -- Servers installed
 lsp.ensure_installed({
-  "clangd",
-  "dockerls",
-  "eslint",
-  "lua_ls",
-  "solargraph",
-  "tsserver",
+  'clangd',
+  'dockerls',
+  'eslint',
+  'lua_ls',
+  'solargraph',
+  'tsserver',
 })
 
 ------------------------------------------------------------------
 -- Key Bindings
 lsp.on_attach(function(_, bufnr)
   local opts = { buffer = bufnr, remap = false }
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<C-/>", function() vim.lsp.buf.signature_help() end, opts)
-  vim.keymap.set("n", "<C-f>", function() vim.lsp.buf.format() end, opts)
+  vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set('n', '<C-h>', function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set('n', '<C-/>', function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set('n', '<C-f>', function() vim.lsp.buf.format() end, opts)
 end)
 
 lsp.setup()
@@ -27,20 +27,25 @@ lsp.setup()
 local cmp = require('cmp')
 
 cmp.setup({
-  -- snippet = {
-  --   expand = function(_)
-  --   end,
-  -- },
+  snippet = {
+    expand = function(args)
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      vim.fn['UltiSnips#Anon'](args.body) -- For `ultisnips` users.
+    end,
+  },
   mapping = {
     -- Configure the desired key mappings for completion navigation
-    ['<C-Space>'] = cmp.mapping.complete(),
+    -- ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-e>'] = cmp.mapping.close(),
+    -- ['<C-e>'] = cmp.mapping.close(),
     ['<Tab>'] = cmp.mapping.select_next_item(),
     ['<S-Tab>'] = cmp.mapping.select_prev_item(),
   },
-  -- sources = {
-  --   -- Add additional sources as needed
-  --   { name = 'nvim_lsp' },
-  -- },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'ultisnips' },
+    { name = 'buffer' },
+  },
 })
+
+vim.g.UltiSnipsSnippetDirectories = { '~/.UltiSnips' }
--- config/nvim/lua/pabloscolpino/packer.lua
+++ config/nvim/lua/pabloscolpino/packer.lua
@@ -25,9 +25,12 @@ return require('packer').startup(function(use)
       },
       { 'williamboman/mason-lspconfig.nvim' }, -- Optional
       -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },                  -- Required
-      { 'hrsh7th/cmp-nvim-lsp' },              -- Required
       { 'L3MON4D3/LuaSnip' },                  -- Required
+      { 'SirVer/ultisnips' },
+      { 'quangnguyen30192/cmp-nvim-ultisnips' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-nvim-lsp' },              -- Required
+      { 'hrsh7th/nvim-cmp' },                  -- Required
     }
   })
 
@@ -36,7 +39,6 @@ return require('packer').startup(function(use)
   use('echasnovski/mini.comment')
   use('lukas-reineke/indent-blankline.nvim') -- Blank lines
   use('tpope/vim-abolish')
-
 
   --[ Git ]---------------------------------------------------------------------
   use('tpope/vim-fugitive')
