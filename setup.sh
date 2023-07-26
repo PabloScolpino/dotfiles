112368b0c8c6332536cccab872ffbd6c85882958
Pablo Scolpino <pabloscolpino@gmail.com>
2023-07-26 19:06:51 -0300

    Fix inventory path reference

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 echo "This is intended to be run by gitpod preconfigurator"
 sudo apt-get install -y --no-install-recommends ansible
-ansible-playbook setup/gitpod.yml -i inventory
+ansible-playbook setup/gitpod.yml -i setup/inventory
