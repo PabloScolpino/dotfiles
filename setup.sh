51f0a2f0871e79081cbf29d63d98807e4d34d325
Pablo Scolpino <pabloscolpino@gmail.com>
2023-07-26 19:21:46 -0300

    make setup.sh script context aware

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 echo "This is intended to be run by gitpod preconfigurator"
+
 sudo apt-get install -y --no-install-recommends python3-setuptools python3-pip python3-wheel
 sudo pip3 install ansible
-ansible-playbook setup/gitpod.yml -i setup/inventory
+
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ansible-playbook $current_dir/setup/gitpod.yml -i $current_dir/setup/inventory
