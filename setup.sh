d72dd1c7b4538b118f937b05457543470c8a9b91
Pablo Scolpino <pabloscolpino@gmail.com>
2023-07-26 19:02:51 -0300

    Try full path playbook reference

--- setup.sh
+++ setup.sh
@@ -2,5 +2,4 @@
 
 echo "This is intended to be run by gitpod preconfigurator"
 sudo apt-get install -y --no-install-recommends ansible
-cd ~/.dotfiles/setup
-ansible-playbook gitpod.yml -i inventory
+ansible-playbook setup/gitpod.yml -i inventory
