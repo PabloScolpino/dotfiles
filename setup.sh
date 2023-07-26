dcabde03e7b1998253356c160ed6e7428f06de0a
Pablo Scolpino <pabloscolpino@gmail.com>
2023-07-26 19:13:31 -0300

    Setup ansible througy python

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 echo "This is intended to be run by gitpod preconfigurator"
-sudo apt-get install -y --no-install-recommends ansible
+sudo apt-get install -y --no-install-recommends python3-setuptools python3-pip python3-wheel
+sudo pip3 install ansible
 ansible-playbook setup/gitpod.yml -i setup/inventory
