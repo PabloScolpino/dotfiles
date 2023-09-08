0a513f4d44db78117fd64a9e374e2a6b7110a85c
Pablo Scolpino <pabloscolpino@gmail.com>
2023-09-08 18:15:54 +0000

    Remove debug output :-)

--- config/ruby.sh
+++ config/ruby.sh
@@ -15,8 +15,6 @@ else
     rubies_path=$(rvm info homes | grep 'ruby:' | cut -d ':' -f 2 | sed 's/\s//g' | sed 's/"//g')
     ruby_in_gems=${gems_path}/bin/ruby
     ruby_in_rubies=${rubies_path}/bin/ruby
-    echo $ruby_in_gems
-    echo $ruby_in_rubies
     if [[ ! -f $ruby_in_gems ]]; then
       ln -s $ruby_in_rubies $ruby_in_gems
     fi
