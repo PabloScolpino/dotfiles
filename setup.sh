ed28cf6a27749ff19139604e99e3a5ad5ff869b1
Pablo Scolpino <pabloscolpino@gmail.com>
2023-10-04 15:41:35 +0000

    Fix python setup for gitpod's setup script

--- setup.sh
+++ setup.sh
@@ -2,8 +2,7 @@
 
 echo "This is intended to be run by gitpod preconfigurator"
 
-sudo apt-get install -y --no-install-recommends python3-setuptools python3-pip python3-wheel
-sudo pip3 install ansible
+pip install ansible
 
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ansible-playbook $current_dir/setup/gitpod.yml -i $current_dir/setup/inventory & disown
