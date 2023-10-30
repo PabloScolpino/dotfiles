42373a2d77a9b2c53f86854df5c8d3c07651daee
Pablo Scolpino <pabloscolpino@gmail.com>
2023-10-30 23:23:12 +0000

    increase telescope live grep width

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,15 +1,37 @@
+local telescope = require('telescope')
+
+telescope.setup(
+  {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = {
+        horizontal = {
+          prompt_position = "top",
+          preview_width = 0.75,
+          results_width = 0.95,
+        },
+        vertical = {
+          mirror = false,
+        },
+        width = 0.9,
+        height = 0.89,
+        preview_cutoff = 120,
+      },
+    }
+  }
+)
+
 local builtin = require('telescope.builtin')
-local t = require('telescope')
 
 vim.keymap.set('n', '<Leader>f', builtin.git_files, { desc = 'Search filename in git project' })
 vim.keymap.set('n', '<Leader>ag', builtin.grep_string, { desc = 'Search for string under cursor' })
 vim.keymap.set('n', '<Leader>af', builtin.live_grep, { desc = 'Live search for input string in content' })
 vim.keymap.set('n', '<Leader>/', builtin.find_files, { desc = 'Search for filename from CWD downwards' })
-vim.keymap.set('n', '<Leader>ap', t.extensions.projects.projects, { desc = 'Show projects' })
+vim.keymap.set('n', '<Leader>ap', telescope.extensions.projects.projects, { desc = 'Show projects' })
 
 vim.keymap.set('n', '<Leader>vo', builtin.vim_options, {})
 vim.keymap.set('n', '<Leader>vk', builtin.keymaps, { desc = 'List vim keymaps' })
 
 vim.keymap.set('n', '<Leader>gl', builtin.git_commits, { desc = 'Show git history with diffs' })
 vim.keymap.set('n', '<Leader>gbl', builtin.git_bcommits, { desc = 'Show git history with diffs for current buffer' })
-vim.keymap.set('n', '<Leader>bb', builtin.buffers,{})
+vim.keymap.set('n', '<Leader>bb', builtin.buffers, {})
