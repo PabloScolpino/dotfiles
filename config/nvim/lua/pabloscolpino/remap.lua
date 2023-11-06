f6e42319f09fe2f7a8bce23fc323282897f97b66
Pablo Scolpino <pabloscolpino@gmail.com>
2023-11-06 20:28:36 +0000

    hotkeys to allow coping with the mouse cleanly

--- config/nvim/lua/pabloscolpino/remap.lua
+++ config/nvim/lua/pabloscolpino/remap.lua
@@ -16,8 +16,22 @@ vim.keymap.set("v", "<C-V>", "v")
 
 --[ Line Numbers ]---------------------------------------------------------
 --noremap <silent> <Leader>N :set relativenumber!<CR>
---noremap <silent> <Leader>c :set number<CR>:GitGutterSignsEnable<CR>
---noremap <silent> <Leader>C :set norelativenumber<CR>:set nonumber<CR>:GitGutterSignsDisable<CR>:SyntasticToggleMode<CR>
+-- vim.keymap.set("n", "<Leader>c", ':set number<CR>:GitGutterSignsEnable<CR>', { desc = "Prepare buffer for mouose copy" })
+-- vim.keymap.set("n", "<Leader>C", ':set norelativenumber<CR>:set nonumber<CR>:GitGutterDisable<CR>',
+--   { desc = "Prepare buffer for mouose copy" })
+vim.keymap.set("n", "<Leader>c", function()
+  vim.wo.number = true
+  vim.cmd("GitGutterEnable")
+end, { desc = "Enable line numbers and GitGutter signs" })
+
+
+vim.keymap.set("n", "<Leader>C", function()
+  vim.wo.relativenumber = false
+  vim.wo.signcolumn = 'no'
+  vim.wo.number = false
+  vim.cmd("GitGutterDisable")
+end, { desc = "Disable GitGutter and line numbers" })
+
 
 vim.keymap.set("n", "<Leader>p", vim.cmd.bprevious, { desc = "jump to previous buffer" })
 vim.keymap.set("n", "<Leader>n", vim.cmd.bnext, { desc = "jump to next buffer" })
