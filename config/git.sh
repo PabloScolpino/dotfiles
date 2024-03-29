d117234342165d3c95e6fc797f991e66897d54ad
Pablo Scolpino <pabloscolpino@gmail.com>
2024-03-29 14:44:25 +0000

    Add autoSetupRemote to git config

--- config/git.sh
+++ config/git.sh
@@ -2,6 +2,7 @@ git config --global alias.up 'pull --rebase --autostash'
 git config --global core.editor "vim"
 git config --global core.excludesfile $HOME/.gitignore_global
 git config --global pull.rebase true
+git config --global push.autoSetupRemote true
 git config --global push.default current
 git config --global user.email 'pabloscolpino@gmail.com'
 git config --global user.name 'Pablo Scolpino'
