7dcca7f6529b0bde5075f0bfae551117bda3d4a0
Pablo Scolpino <pabloscolpino@gmail.com>
2024-09-09 15:08:18 +0000

    Set python version through pyenv

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ set -ex
 
 echo "This is intended to be run by gitpod preconfigurator"
 
+pyenv global 3.12.4
 pip install ansible
 
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
