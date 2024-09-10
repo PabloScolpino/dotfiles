5941668e408bbf8ee72dbae3b01220e67df971db
Pablo Scolpino <pabloscolpino@gmail.com>
2024-09-10 21:40:33 +0000

    only add paths that exist to path

--- config/path.sh
+++ config/path.sh
@@ -1,13 +1,31 @@
-if [ -n ${HOMEBREW_PREFIX} ]
-then
-  export PATH="$HOMEBREW_PREFIX/bin:$PATH"
+if [ -n "${HOMEBREW_PREFIX}" ] && [ -d "${HOMEBREW_PREFIX}/bin" ]; then
+    export PATH="${HOMEBREW_PREFIX}/bin:$PATH"
 fi
 
-export PATH="$HOME/bin:$PATH"
-export PATH="$HOME/.dotfiles/bin:$PATH"
-export PATH="$HOME/.asdf/bin:$PATH"
-export PATH="$HOME/.cargo/bin:$PATH"
-export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
-export PATH="$HOME/.krew/bin:$PATH"
-export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/.krew/bin:$PATH"
+if [ -d "$HOME/bin" ]; then
+    export PATH="$HOME/bin:$PATH"
+fi
+
+if [ -d "$HOME/.dotfiles/bin" ]; then
+    export PATH="$HOME/.dotfiles/bin:$PATH"
+fi
+
+if [ -d "$HOME/.asdf/bin" ]; then
+    export PATH="$HOME/.asdf/bin:$PATH"
+fi
+
+if [ -d "$HOME/.cargo/bin" ]; then
+    export PATH="$HOME/.cargo/bin:$PATH"
+fi
+
+if [ -d "/opt/homebrew/opt/libpq/bin" ]; then
+    export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+fi
+
+if [ -d "$HOME/.krew/bin" ]; then
+    export PATH="$HOME/.krew/bin:$PATH"
+fi
+
+if [ -d "$HOME/.local/bin" ]; then
+    export PATH="$HOME/.local/bin:$PATH"
+fi
