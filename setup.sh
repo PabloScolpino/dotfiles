717d7c5efb19bc990c209c5d56f1d4ba5d126e6f
Pablo Scolpino <pabloscolpino@gmail.com>
2024-09-10 15:35:31 +0000

    try setting access to bash_profile again

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ echo "This is intended to be run by gitpod preconfigurator"
 
 echo "this is being run by: $(whoami)"
 
+sudo chown gitpod ~/.bash_profile
+echo -e "\nif [ -f ~/.bashrc ]; then\n    source ~/.bashrc\nfi" >> ~/.bash_profile
+
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
@@ -14,6 +17,3 @@ pip install ansible
 
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ansible-playbook $current_dir/setup/gitpod.yml -i $current_dir/setup/inventory & disown
-
-chown gitpod ~/.bash_profile
-echo -e "\nif [ -f ~/.bashrc ]; then\n    source ~/.bashrc\nfi" >> ~/.bash_profile
