da23d47f4e3d081eff9025a3fb56317a535f7780
Pablo Scolpino <pabloscolpino@gmail.com>
2024-09-11 16:01:02 +0000

    Add itallics support to gitpod

--- setup.sh
+++ setup.sh
@@ -6,10 +6,25 @@ echo "This is intended to be run by gitpod preconfigurator"
 
 echo "this is being run by: $(whoami)"
 
+################################################################################
+# Ensure .bashrc is run on every new term
 touch ~/.bash_profile
 sudo chown gitpod ~/.bash_profile
 echo -e "\nif [ -f ~/.bashrc ]; then\n    source ~/.bashrc\nfi" >> ~/.bash_profile
 
+################################################################################
+# Add itallics support to the terminal
+cat << EOF > xterm-256color-italic.terminfo
+xterm-256color-italic|xterm with 256 colors and italic,
+    sitm=\E[3m, ritm=\E[23m, use=xterm-256color,
+EOF
+
+tic xterm-256color-italic.terminfo
+
+echo 'export TERM=xterm-256color-italic' >> ~/.bashrc
+
+################################################################################
+# Configure pyenv and global python version
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
