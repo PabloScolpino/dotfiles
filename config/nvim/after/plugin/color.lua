2da540bae089502069417fa718e5ec5a602b133e
Pablo Scolpino <pabloscolpino@gmail.com>
2024-09-11 16:45:28 +0000

    Dont use italic in comments...

--- config/nvim/after/plugin/color.lua
+++ config/nvim/after/plugin/color.lua
@@ -5,6 +5,7 @@ function apply_color_scheme(color)
 
   vim.api.nvim_set_hl(0, "Normal", { bg = "black" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "black" })
+  vim.api.nvim_set_hl(0, "Comment", { fg = "grey", bg = "NONE" })
 end
 
 apply_color_scheme()
