bf7f9e330be177d96be4fcd73ce254b09cb9b1cd
Pablo Scolpino <pabloscolpino@gmail.com>
2025-04-08 12:23:25 -0300

    tweak trouble configuration to be ont he side

--- config/nvim/lua/pabloscolpino/plugins/UI.lua
+++ config/nvim/lua/pabloscolpino/plugins/UI.lua
@@ -86,15 +86,27 @@ return {
   -- }
   {
     "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    opts = {
+      modes = {
+        on_the_side = {
+          mode = "diagnostics",
+          win = {
+            type = "split",
+            position = 'left',
+          },
+          filter = { buf = 0 }, -- filter diagnostics to the current buffer
+          focus = true,
+        },
+      }
+    }, -- for default options, refer to the configuration section for custom setup.
     cmd = "Trouble",
     keys = {
-      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",                        desc = "Diagnostics (Trouble)", },
-      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",           desc = "Buffer Diagnostics (Trouble)", },
-      { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>",                desc = "Symbols (Trouble)", },
-      { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)", },
-      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>",                            desc = "Location List (Trouble)", },
-      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>",                             desc = "Quickfix List (Trouble)", },
+      { "<leader>xx", "<cmd>Trouble on_the_side toggle<cr>",              desc = "Diagnostics (Trouble)", },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)", },
+      { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>",      desc = "Symbols (Trouble)", },
+      -- { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)", },
+      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>",                  desc = "Location List (Trouble)", },
+      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>",                   desc = "Quickfix List (Trouble)", },
     },
   }
 }
