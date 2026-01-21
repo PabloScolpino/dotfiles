72c4be45a80a4629376cb545d78146f2c78403c2
Pablo Scolpino <pabloscolpino@gmail.com>
2026-01-21 16:13:14 -0300

    make snippets great again

--- config/nvim/lua/pabloscolpino/plugins/lsp.lua
+++ config/nvim/lua/pabloscolpino/plugins/lsp.lua
@@ -36,13 +36,12 @@ return {
     dependencies = {
       { 'onsails/lspkind.nvim' },
       'hrsh7th/cmp-buffer',
-      'SirVer/ultisnips',
       {
-        'quangnguyen30192/cmp-nvim-ultisnips',
-        config = function()
-          -- optional call to setup (see customization section)
-          require("cmp_nvim_ultisnips").setup {}
-        end,
+        'garymjr/nvim-snippets',
+        opts = {
+          friendly_snippets = true,
+          search_paths = { vim.fn.stdpath('config') .. '/snippets' },
+        },
       }
       -- {
       --   'codota/tabnine-nvim',
@@ -72,33 +71,33 @@ return {
 
       cmp.setup({
         snippet = {
-          -- REQUIRED - you must specify a snippet engine
           expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-            -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-
-            -- For `mini.snippets` users:
-            -- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
-            -- insert({ body = args.body }) -- Insert at cursor
-            -- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
-            -- require("cmp.config").set_onetime({ sources = {} })
+            vim.snippet.expand(args.body)
           end,
         },
-        -- snippet = {
-        --   expand = function(args)
-        --     vim.snippet.expand(args.body)
-        --   end,
-        -- },
         window = {
           -- completion = cmp.config.window.bordered(),
           -- documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ['<Tab>'] = cmp.mapping.select_next_item(),
-          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if vim.snippet.active({ direction = 1 }) then
+              vim.snippet.jump(1)
+            elseif cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if vim.snippet.active({ direction = -1 }) then
+              vim.snippet.jump(-1)
+            elseif cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
           ['<C-y>'] = cmp.mapping.confirm({ select = true }),
           ['<C-u>'] = cmp.mapping.scroll_docs(-4),
           ['<C-d>'] = cmp.mapping.scroll_docs(4),
@@ -110,7 +109,7 @@ return {
           { name = 'copilot' },
           { name = 'nvim_lsp' },  -- For vsnip users.
           -- { name = 'luasnip' }, -- For luasnip users.
-          { name = 'ultisnips' }, -- For ultisnips users.
+          { name = 'snippets' },
           -- { name = 'snippy' }, -- For snippy users.
           { name = 'buffer' }, })
       })
