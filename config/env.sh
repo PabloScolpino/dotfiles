4f1e3d02608022194e40cb6e0399687c6652ca63
Pablo Scolpino <pabloscolpino@gmail.com>
2026-03-16 11:04:52 -0300

    refactor linux conditions

--- config/env.sh
+++ config/env.sh
@@ -26,3 +26,5 @@ export GPG_TTY=$(tty)
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -l -g ""'
 
 export SSH_AUTH_SOCK="$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
+unset GEM_HOME # fix tmuxinator polluting env
