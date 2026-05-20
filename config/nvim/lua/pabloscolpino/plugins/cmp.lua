8cd6deb920d2220f4f3b5121ec237dfcb82fefb3
Pablo Scolpino <pabloscolpino@gmail.com>
2026-05-20 00:02:21 -0300

    Fix nvim 0.12 deprecations (#19)
    * update plugins
    
    * Migrate LSP capabilities to vim.lsp.config('*')
    
    Drop lspconfig.util.default_config in favor of the 0.11+ vim.lsp.config
    API. cmp_nvim_lsp.default_capabilities() already merges with
    make_client_capabilities() internally, so no manual tbl_deep_extend.
    
    * Drop reach.nvim, rebind buffer-jump to bufferline
    
    reach.nvim abandoned since 2022 and emits vim.validate{} deprecation
    warning on nvim 0.12. Replace its switch_to_buffer keymaps with
    BufferLineGoToBuffer on the existing bufferline.nvim. Indexes now match
    visible bufferline tab positions (1..4) instead of reach's handle
    ordering. <C-h> mapped to BufferLineCyclePrev for left-tab cycling.
    
    Run :Lazy clean to regenerate lazy-lock.json (separate commit).
    
    * update plugins
    
    * switch to blink

---
+++ config/nvim/lua/pabloscolpino/plugins/cmp.lua
@@ -0,0 +1,68 @@
+return {
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup({
+        suggestion = { enabled = false, auto_trigger = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
+  {
+    'fang2hou/blink-copilot',
+  },
+  {
+    'saghen/blink.cmp',
+    version = '*',
+    event = 'InsertEnter',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      'zbirenbaum/copilot.lua',
+      'fang2hou/blink-copilot',
+    },
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = {
+        preset = 'none',
+        ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
+        ['<C-y>'] = { 'select_and_accept', 'fallback' },
+        ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide', 'fallback' },
+      },
+      snippets = { preset = 'default' },
+      sources = {
+        default = { 'copilot', 'lsp', 'snippets', 'buffer', 'path' },
+        providers = {
+          copilot = {
+            name = 'copilot',
+            module = 'blink-copilot',
+            score_offset = 100,
+            async = true,
+          },
+          snippets = {
+            opts = {
+              friendly_snippets = true,
+              search_paths = { vim.fn.stdpath('config') .. '/snippets' },
+            },
+          },
+        },
+      },
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+          treesitter_highlighting = false,
+        },
+        ghost_text = { enabled = false },
+      },
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
+    },
+  },
+}
--- config/nvim/lua/pabloscolpino/plugins/lsp.lua
+++ config/nvim/lua/pabloscolpino/plugins/lsp.lua
@@ -5,122 +5,11 @@ return {
     opts = {},
   },
   {
-    "zbirenbaum/copilot-cmp",
-    dependencies = {
-      {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        event = "InsertEnter",
-        config = function()
-          require("copilot").setup({
-            -- panel = {
-            --   enabled = true,
-            -- },
-            -- suggestion = {
-            --   auto_trigger = true,
-            --   hide_during_completion = true,
-            --   keymap = {
-            --     accept = '<C-yy>',
-            --   },
-            -- },
-          })
-        end,
-      },
-    },
-    config = function()
-      require("copilot_cmp").setup()
-    end,
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      { 'onsails/lspkind.nvim' },
-      'hrsh7th/cmp-buffer',
-      {
-        'garymjr/nvim-snippets',
-        opts = {
-          friendly_snippets = true,
-          search_paths = { vim.fn.stdpath('config') .. '/snippets' },
-        },
-      }
-      -- {
-      --   'codota/tabnine-nvim',
-      --   build = "./dl_binaries.sh"
-      -- },
-      -- {
-      --   'tzachar/cmp-tabnine',
-      --   build = './install.sh',
-      --   dependencies = 'hrsh7th/nvim-cmp',
-      --   config = function()
-      --     require('cmp_tabnine.config'):setup({
-      --       max_lines = 1000,
-      --       max_num_results = 20,
-      --       sort = true,
-      --       run_on_every_keystroke = true,
-      --       snippet_placeholder = '..',
-      --       ignored_file_types = {},
-      --       show_prediction_strength = true
-      --     })
-      --   end
-      -- }
-    },
-
-    event = 'InsertEnter',
-    config = function()
-      local cmp = require('cmp')
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body)
-          end,
-        },
-        window = {
-          -- completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if vim.snippet.active({ direction = 1 }) then
-              vim.snippet.jump(1)
-            elseif cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if vim.snippet.active({ direction = -1 }) then
-              vim.snippet.jump(-1)
-            elseif cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-        }),
-        formatting = {
-          format = require('lspkind').cmp_format()
-        },
-        sources = cmp.config.sources({
-          { name = 'copilot' },
-          { name = 'nvim_lsp' },  -- For vsnip users.
-          -- { name = 'luasnip' }, -- For luasnip users.
-          { name = 'snippets' },
-          -- { name = 'snippy' }, -- For snippy users.
-          { name = 'buffer' }, })
-      })
-    end
-  },
-  {
     'neovim/nvim-lspconfig',
     cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
     event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
-      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'saghen/blink.cmp' },
       { 'williamboman/mason.nvim' },
       { 'williamboman/mason-lspconfig.nvim' },
     },
@@ -130,15 +19,9 @@ return {
       vim.opt.signcolumn = 'yes'
     end,
     config = function()
-      local lsp_defaults = require('lspconfig').util.default_config
-
-      -- Add cmp_nvim_lsp capabilities settings to lspconfig
-      -- This should be executed before you configure any language server
-      lsp_defaults.capabilities = vim.tbl_deep_extend(
-        'force',
-        lsp_defaults.capabilities,
-        require('cmp_nvim_lsp').default_capabilities()
-      )
+      vim.lsp.config('*', {
+        capabilities = require('blink.cmp').get_lsp_capabilities(),
+      })
 
       -- LspAttach is where you enable features that only work
       -- if there is a language server active in the file
