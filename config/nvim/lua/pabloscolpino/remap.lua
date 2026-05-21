3e79be643ab8927f0af67ba9c2f1805d84eae4c5
Pablo Scolpino <pabloscolpino@gmail.com>
2026-05-20 21:21:42 -0300

    Drop duplicated v/CTRL-V swap keymaps
    Lines 10 and 12 were exact copies of lines 9 and 11. The final
    vim.keymap.set call wins anyway, so behavior is unchanged; this is
    pure cleanup.

--- config/nvim/lua/pabloscolpino/remap.lua
+++ config/nvim/lua/pabloscolpino/remap.lua
@@ -7,8 +7,6 @@ vim.keymap.set("n", ":", ";")
 
 --[ Swap v and CTRL-V, because Block mode is more useful that Visual ]-----
 vim.keymap.set("n", "v", "<C-V>")
-vim.keymap.set("n", "v", "<C-V>")
-vim.keymap.set("n", "<C-V>", "v")
 vim.keymap.set("n", "<C-V>", "v")
 
 vim.keymap.set("v", "v", "<C-V>")
